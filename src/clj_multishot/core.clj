d4eef1b67414d1628af843c130a8aefd1c9bfb29
Glorphindale <glorphindale@gmail.com>
2013-09-13 22:16:45 +0700

    Refactor code.

--- src/clj_multishot/core.clj
+++ src/clj_multishot/core.clj
@@ -6,27 +6,24 @@
 (def downstreams
   ["http://localhost:8090" "http://localhost:8091"])
 
-(defn forward-request [req server]
+(defn forward-request
+  "take a Ring-compatible request map, forward it to the specified server
+  and return the response"
+  [req server]
   (let [{:keys [uri request-method headers query-string body]} req
         full-url (str server
                       uri
-                      (#(if % (str "&" %)) query-string))
-        result (client/request {:method request-method
-                                :url full-url
-                                :headers (dissoc headers "content-length")
-                                :body body})]
-    result))
+                      (#(if % (str "?" %)) query-string))]
+    (client/request {:method request-method
+                     :url full-url
+                     :headers (dissoc headers "content-length")
+                     :body body})))
 
 (defn app [req]
   (println "Received request")
   (doseq [downstream (rest downstreams)]
-    (println "Sending to" downstream)
     (future (forward-request req downstream)))
-
-  (let [result (forward-request req (first downstreams))]
-    {:status 200
-     :headers (:headers result)
-     :body (:body result)}))
+  (forward-request req (first downstreams)))
 
 (defn -main []
     (jetty/run-jetty app {:port 8080}))
